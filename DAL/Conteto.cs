e2aef6a46208a83f13922b42da5d31663baf20a7
JoseangelVargaspolaco <97201605+JoseangelVargaspolaco@users.noreply.github.com>
2022-10-03 18:09:31 -0400

    Comit de inicio

--- DAL/Conteto.cs
+++ DAL/Conteto.cs
@@ -3,9 +3,10 @@ using Microsoft.EntityFrameworkCore;
 
 namespace AP1_P1_Jose_angel_Vargas_Polanco.DAL
 {
+    
     public class Contexto : DbContext 
     {
-       public DbSet<Registros>  Registros { get; set; }
+       public DbSet<Registros>  Registros { get; set; } 
 
        public Contexto(DbContextOptions<Contexto> options) : base(options)
        {
